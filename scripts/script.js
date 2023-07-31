1f1dff78142be017ae7d1e3b1f3c147645d48cbf
demaxio <demaxio@mail.ru>
2023-08-01 00:57:55 +0400

    beta-1
    pc version complited

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
-    slidesPerGroup:3,
+    slidesPerGroup: 3,
     loop: true,
     spaceBetween: 10,
     // autoplay: {
@@ -30,4 +30,36 @@ var swiper = new Swiper(".mySwiper", {
             spaceBetween: 32,
         },
     },
+});
+$(".responsive").slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
 });
